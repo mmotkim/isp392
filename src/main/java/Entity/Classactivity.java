f0f881214ae0c9ac07cb15345f7a42d2c65f150a
ducnhhe176187 <ducnhhe176187@fpt.edu.vn>
2023-07-10 08:28:47 +0700

    Entities

---
+++ src/main/java/Entity/Classactivity.java
@@ -0,0 +1,67 @@
+package Entity;
+
+import jakarta.persistence.*;
+
+import java.sql.Date;
+
+@Entity
+@IdClass(ClassactivityPK.class)
+public class Classactivity {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "class_id", nullable = false)
+    private int classId;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "activity_id", nullable = false)
+    private int activityId;
+    @Basic
+    @Column(name = "date", nullable = true)
+    private Date date;
+
+    public int getClassId() {
+        return classId;
+    }
+
+    public void setClassId(int classId) {
+        this.classId = classId;
+    }
+
+    public int getActivityId() {
+        return activityId;
+    }
+
+    public void setActivityId(int activityId) {
+        this.activityId = activityId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Classactivity that = (Classactivity) o;
+
+        if (classId != that.classId) return false;
+        if (activityId != that.activityId) return false;
+        if (date != null ? !date.equals(that.date) : that.date != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = classId;
+        result = 31 * result + activityId;
+        result = 31 * result + (date != null ? date.hashCode() : 0);
+        return result;
+    }
+}
